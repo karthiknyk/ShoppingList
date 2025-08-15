fcaa18356be1e8f87f69d406f35969991f818355
karthik <mindroittech@gmail.com>
2025-08-16 01:09:46 +0530

    eas build apk

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+// app/index.tsx
+import { useRouter } from 'expo-router';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Shopping List App</Text>
+    </View>
+  );
+}
